1ad6e551a2527e8d2aff0f9c636a7f866490e4f3
Aicos <richard.sw268@gmail.com>
2021-01-26 18:33:48 -0500

    Creando componentes

---
+++ src/top-card-list.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+function TopCardList(){
+    return(
+        <section className="top-cards">
+    <div className="wrapper">
+      <div className="grid">
+        <article className="card facebook">
+          <p className="card-title">
+            <img src="images/icon-facebook.svg" alt="" />
+            @Aicos
+          </p>
+          <p className="card-followers">
+            <span className="card-followers-number">1483</span>
+            <span className="card-followers-title">Followers</span>
+          </p>
+          <p className="card-today">
+            <img src="images/icon-up.svg" alt="" />
+            12 Today
+          </p>
+        </article>
+        <article className="card twitter">
+          <p className="card-title">
+            <img src="images/icon-twitter.svg" alt="" />
+            @Aicos
+          </p>
+          <p className="card-followers">
+            <span className="card-followers-number">28k</span>
+            <span className="card-followers-title">Followers</span>
+          </p>
+          <p className="card-today">
+            <img src="images/icon-up.svg" alt="" />
+            12 Today
+          </p>
+        </article>
+        <article className="card instagram">
+          <p className="card-title">
+            <img src="images/icon-instagram.svg" alt="" />
+            @Aicos
+          </p>
+          <p className="card-followers">
+            <span className="card-followers-number">6k</span>
+            <span className="card-followers-title">Followers</span>
+          </p>
+          <p className="card-today">
+            <img src="images/icon-up.svg" alt="" />
+            12 Today
+          </p>
+        </article>
+        <article className="card youtube">
+          <p className="card-title">
+            <img src="images/icon-youtube.svg" alt="" />
+            @Aicos
+          </p>
+          <p className="card-followers">
+            <span className="card-followers-number">12k</span>
+            <span className="card-followers-title">Followers</span>
+          </p>
+          <p className="card-today">
+            <img src="images/icon-up.svg" alt="" />
+            12 Today
+          </p>
+        </article>
+      </div>
+    </div>
+  </section>
+    )
+}
+
+export default TopCardList;
